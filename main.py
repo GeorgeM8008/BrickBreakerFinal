9d2c3299c07d5d387beea3b4e69c1fc0bb166923
GeorgeM8008 <124279891+GeorgeM8008@users.noreply.github.com>
2023-06-07 10:56:29 +0000

    Finished comments

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
+##George Mi, CS30, May 23th
+
+#Importing all the modules I need
 import pygame
 import math
+#Importing the Ball class from the other file
 from Calc_ball import Ball
 import Calc_ball
 
 #Initializes all imported pygame modules
 pygame.init()
 
+#Font and size of letters that are being printed on the screen
 LIVES_FONT = pygame.font.SysFont("ariel.ttf", 42)
+#Number of lives the player has
 lives = 3
 
+#Dimensions of the screen and initializing it
 screen_width = 700
 screen_height = 500
 screen = pygame.display.set_mode((screen_width, screen_height))
 
+#Variable running that beings as false, you need to click to start
 running = False
 
 #Sets the header as Brick Breaker
@@ -31,6 +39,8 @@ playerY = 460
 player_width = 125
 player_height = 18
 
+'''Player class includes drawing the platform and it's movement based on
+the mouse. Also set's boundaries for the player.'''
 class Player:
     def __init__(self, x, y, width, height, colour):
         self.x = x
@@ -53,6 +63,9 @@ class Player:
             self.x = 0
         elif self.x >= 670:
             self.x = 670
+
+'''Brick class includes drawing an individual brick, detecting a collision,
+and changing colour based on it's health'''
 class Brick:
     def __init__(self, x, y, width, height, health, colours):
         self.x = x
@@ -81,17 +94,23 @@ class Brick:
         self.health -= 1
         self.colour = self.interpolate(
             *self.colours, self.health/self.max_health)
-
     @staticmethod
     def interpolate(color_a, color_b, t):
         # 'color_a' and 'color_b' are RGB tuples
         # 't' is a value between 0.0 and 1.0
         # this is a naive interpolation
         return tuple(int(a + (b - a) * t) for a, b in zip(color_a, color_b))
-
+      
+#The ball
 ball = Ball(Calc_ball.iballX, Calc_ball.iballY, Calc_ball.ball_radius, (0, 0, 0))
+
+#The platform
 player = Player(playerX, playerY, player_width, player_height, (0, 0, 0))
 
+#The bricks
+bricks = generate_bricks(3, 10)
+
+#Resets the ball after it hits the floor
 def reset():
     global lives, running
     pygame.time.delay(500)
@@ -100,12 +119,15 @@ def reset():
     ball.y = playerY - 10
     ball.set_vel(0, ball.VEL * -1)
 
+#Displays text if you lose
 def display_text(text):
     text_render = LIVES_FONT.render(text, 1, "red")
     screen.blit(text_render, (screen_width/2 - text_render.get_width() /
                                2, screen_height/2 - text_render.get_height()/2))
     pygame.display.update()
     pygame.time.delay(3000)
+
+#Ball collision with walls, flips it's velocities
 def ball_col(ball):
     global running, lives
     if ball.x - Calc_ball.ball_radius <= 0 or ball.x + Calc_ball.ball_radius >= screen_width - 10:
@@ -115,6 +137,7 @@ def ball_col(ball):
     if ball.y + ball.radius >= 500:
         reset()
 
+'''Ball collision with player platform, changes velocity at different angeles depending on where it hit the platform'''
 def ball_player_col(ball, player):
     if not (ball.x <= player.x + player.width and ball.x >= player.x):
         return
@@ -133,6 +156,7 @@ def ball_player_col(ball, player):
 
     ball.set_vel(x_vel, y_vel)
 
+#Creates bricks in rows and columns
 def generate_bricks(rows, cols):
     gap = 2
     brick_width = screen_width // cols - gap
@@ -146,10 +170,8 @@ def generate_bricks(rows, cols):
             bricks.append(brick)
     return bricks
 
-ball = Ball(Calc_ball.iballX, Calc_ball.iballY, Calc_ball.ball_radius, (0, 0, 0))
-player = Player(playerX, playerY, player_width, player_height, (0, 0, 0))
-bricks = generate_bricks(3, 10)
-
+'''Before running is true, it displays all the components frozen until
+the player clicks their mouse'''
 while running == False:
     screen.fill((245, 245, 220))
   
@@ -168,7 +190,9 @@ while running == False:
             running = True
     clock.tick(FPS)
     pygame.display.update()
-  
+
+'''When running, goes through all the functions until either all the bricks are gone
+or the player runs out of lives'''
 while running == True:
     screen.fill((245, 245, 220))
     player.draw(screen)
@@ -201,6 +225,7 @@ while running == True:
 
     clock.tick(FPS)
 
-
+#Continually updates the screen
     pygame.display.update()
+#Pygame quits if running ever becomes false again
 pygame.quit()
