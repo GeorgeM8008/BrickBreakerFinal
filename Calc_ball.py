24522f48c48ec6b8924ae37cc15698a5cdf47b9b
GeorgeM8008 <124279891+GeorgeM8008@users.noreply.github.com>
2023-06-07 10:13:34 +0000

    Bricks work

--- Calc_ball.py
+++ Calc_ball.py
@@ -1,23 +1,34 @@
 import pygame
-import main
+
+#All screen variables and playerY are here to prevent circular importing
+screen_width = 725
+screen_height = 500
+screen = pygame.display.set_mode((screen_width, screen_height))
+playerY = 460
 
 ball_radius = 10
-
-class ball:
+class Ball:
     VEL = 5
-    def __init__(self, x, y, radius, colour):
+
+    def __init__(self, x, y, radius, color):
         self.x = x
         self.y = y
         self.radius = radius
-        self.colour = colour
+        self.color = color
         self.x_vel = 0
         self.y_vel = -self.VEL
 
     def move(self):
         self.x += self.x_vel
         self.y += self.y_vel
+
     def set_vel(self, x_vel, y_vel):
         self.x_vel = x_vel
         self.y_vel = y_vel
-    def draw(self):
-        pygame.draw.circle(main.screen, self.colour, (self.x, self.y), self.radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+
+
+iballX = screen_width/2 - ball_radius
+iballY = playerY - 10
--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import pygame
 import math
+from Calc_ball import Ball
+import Calc_ball
 
-
+#Initializes all imported pygame modules
 pygame.init()
 
 
-screen_width = 725
+screen_width = 700
 screen_height = 500
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-rows = 6
-cols = 6
+running = False
 
-
+#Sets the header as Brick Breaker
 pygame.display.set_caption("Brick Breaker")
+#Sets the icon as an image of Brick Breaker
 icon = pygame.image.load('brick.png')
 pygame.display.set_icon(icon)
 
@@ -21,6 +23,7 @@ pygame.display.set_icon(icon)
 FPS = 60
 clock = pygame.time.Clock()
 
+#Initial position and size of the platform
 playerX = 293
 playerY = 460
 player_width = 125
@@ -44,42 +47,51 @@ class Player:
                self.x = mouse_Pos[0] - 62
                player.draw(screen)
 
-        if self.x <= 55:
-            self.x = 55
+        if self.x <= 0:
+            self.x = 0
         elif self.x >= 670:
             self.x = 670
-
-ball_radius = 10
-class Ball:
-    VEL = 5
-
-    def __init__(self, x, y, radius, color):
+class Brick:
+    def __init__(self, x, y, width, height, health, colours):
         self.x = x
         self.y = y
-        self.radius = radius
-        self.color = color
-        self.x_vel = 0
-        self.y_vel = -self.VEL
-
-    def move(self):
-        self.x += self.x_vel
-        self.y += self.y_vel
-
-    def set_vel(self, x_vel, y_vel):
-        self.x_vel = x_vel
-        self.y_vel = y_vel
+        self.width = width
+        self.height = height
+        self.health = health
+        self.max_health = health
+        self.colours = colours
+        self.colour = colours[0]
 
     def draw(self, screen):
-        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+        pygame.draw.rect(screen, self.colour, (self.x, self.y, self.width, self.height))
 
+    def collide(self, ball):
+        if not (ball.x <= self.x + self.width and ball.x >= self.x):
+            return False
+        if not (ball.y - ball.radius <= self.y + self.height):
+            return False
 
-iballX = screen_width/2 - ball_radius
-iballY = playerY - 10
-ball = Ball(iballX, iballY, ball_radius, (0, 0, 0))
+        self.hit()
+        ball.set_vel(ball.x_vel, ball.y_vel * -1)
+        return True
+
+    def hit(self):
+        self.health -= 1
+        self.colour = self.interpolate(
+            *self.colours, self.health/self.max_health)
+
+    @staticmethod
+    def interpolate(color_a, color_b, t):
+        # 'color_a' and 'color_b' are RGB tuples
+        # 't' is a value between 0.0 and 1.0
+        # this is a naive interpolation
+        return tuple(int(a + (b - a) * t) for a, b in zip(color_a, color_b))
+
+ball = Ball(Calc_ball.iballX, Calc_ball.iballY, Calc_ball.ball_radius, (0, 0, 0))
 player = Player(playerX, playerY, player_width, player_height, (0, 0, 0))
 
 def ball_col(ball):
-    if ball.x - ball_radius <= 0 or ball.x + ball_radius >= screen_width - 10:
+    if ball.x - Calc_ball.ball_radius <= 0 or ball.x + Calc_ball.ball_radius >= screen_width - 10:
         ball.set_vel(ball.x_vel * -1, ball.y_vel)
     if ball.y + ball.radius >= screen_height or ball.y - ball.radius <= 0:
         ball.set_vel(ball.x_vel, ball.y_vel * -1)
@@ -101,14 +113,34 @@ def ball_player_col(ball, player):
     y_vel = math.cos(angle_radians) * ball.VEL * -1
 
     ball.set_vel(x_vel, y_vel)
-       
-       
+
+def generate_bricks(rows, cols):
+    gap = 2
+    brick_width = screen_width // cols - gap
+    brick_height = 20
+
+    bricks = []
+    for row in range(rows):
+        for col in range(cols):
+            brick = Brick(col * brick_width + gap * col, row * brick_height +
+                          gap * row, brick_width, brick_height, 2, [(0, 0, 255), (255, 255, 0)])
+            bricks.append(brick)
+    return bricks
+
+ball = Ball(Calc_ball.iballX, Calc_ball.iballY, Calc_ball.ball_radius, (0, 0, 0))
+player = Player(playerX, playerY, player_width, player_height, (0, 0, 0))
+bricks = generate_bricks(3, 10)
 
 running = False
 while running == False:
     screen.fill((245, 245, 220))
+  
     player.draw(screen)
     ball.draw(screen)
+  
+    for brick in bricks:
+        brick.draw(screen)
+      
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             running = True
@@ -123,6 +155,15 @@ while running:
     ball_col(ball)
     ball_player_col(ball, player)
     ball.draw(screen)
+    for brick in bricks:
+        brick.draw(screen)
+
+    for brick in bricks[:]:
+        brick.collide(ball)
+
+        if brick.health <= 0:
+            bricks.remove(brick)
+            
   
     clock.tick(FPS)
 
